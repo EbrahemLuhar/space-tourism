619def5089986e94c393611fef22f3788543c005
Ebrahem Luhar <eb.luhar@gmail.com>
2024-02-04 16:49:52 +0000

    Theme colours and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colours: {
+				'primary': '#0B0D17',
+				'secondary': '#D0D6F9',
+			},
+			fontFamily: {
+				'sans': ['Barlow', 'sans-serif'],
+				'serif': ['Bellefair', 'Georgia'],
+				'barlow-condensed': ['"Barlow Condensed"', 'Georgia'],
+			}
+		},
 	},
 	plugins: [],
 }
